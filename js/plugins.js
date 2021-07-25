286876527e41f43e3951a45a427c5c8519ee8026
Rasedul <raselr699@gmail.com>
2021-07-25 12:16:22 +0600

    testimonial section carousel add

--- js/plugins.js
+++ js/plugins.js
@@ -22,3 +22,21 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+// owl carousel js function hare
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:15,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:2
+      }
+  }
+})
